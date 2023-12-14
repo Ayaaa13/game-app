bab6edac67ee65e8bad7c19d58822b8a59f58204
Ayaaa13 <aiahnava5@gmail.com>
2023-12-14 10:31:10 +0800

    Update login js

--- js/login.js
+++ js/login.js
@@ -11,12 +11,10 @@ const userData = [
 ];
 
 const usernameError = () => {
-  errorusername.classList.add("wrong");
   usernameAccount.classList.add("wrong");
 };
 
 const passwordError = () => {
-  errorpassword.classList.add("wrong");
   passwordAccount.classList.add("wrong");
 };
 
@@ -28,21 +26,67 @@ const passwordCorrect = () => {
   passwordAccount.classList.add("correct");
 };
 
-loginBtn.addEventListener("click", function (e) {
-  e.preventDefault();
+const Login = () => {
+  const loginBtn = document.querySelector(".form-loginBtn");
+  const usernameAccount = document.querySelector("#username");
+  const passwordAccount = document.querySelector("#password");
+  const errorusername = document.querySelector(".error-username");
+  const errorpassword = document.querySelector(".error-password");
 
-  const usernameInput = usernameAccount.value;
-  const passwordInput = passwordAccount.value;
+  const userData = [
+    { username: "user1", password: "pass1" },
+    { username: "user2", password: "pass2" },
+    // Add more user data as needed
+  ];
 
-  const user = userData.find(
-    (user) => user.username === usernameInput && user.password === passwordInput
-  );
+  const usernameError = () => {
+    usernameAccount.classList.add("wrong");
+    usernameAccount.classList.remove("correct");
+  };
 
-  if (!user) {
-    usernameError();
-    passwordError();
-  } else {
-    usernameCorrect();
-    passwordCorrect();
-  }
-});
+  const passwordError = () => {
+    passwordAccount.classList.add("wrong");
+    passwordAccount.classList.remove("correct");
+  };
+
+  const usernameCorrect = () => {
+    usernameAccount.classList.add("correct");
+    usernameAccount.classList.remove("wrong");
+  };
+
+  const passwordCorrect = () => {
+    passwordAccount.classList.add("correct");
+    passwordAccount.classList.remove("wrong");
+  };
+
+  loginBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const usernameInput = usernameAccount.value;
+    const passwordInput = passwordAccount.value;
+
+    const username = userData.find((user) => user.username === usernameInput);
+    const password = userData.find((user) => user.password === passwordInput);
+    if (!username) {
+      usernameError();
+    }
+
+    if (!password) {
+      passwordError();
+    }
+
+    if (username) {
+      usernameCorrect();
+    }
+
+    if (password) {
+      passwordCorrect();
+    }
+
+    if (username && password) {
+      window.location.href = "/pages/home.html";
+    }
+  });
+};
+
+Login();
