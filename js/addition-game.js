8b97ed951eeff4625ac0d12ade616c07934efcde
Ayaaa13 <aiahnava5@gmail.com>
2023-12-16 14:46:11 +0800

    Update game js and css

--- js/addition-game.js
+++ js/addition-game.js
@@ -1,60 +1,79 @@
 const Timer = document.querySelector(".game-timer");
-const startBtn = document.querySelector(".start-button")
+const startBtn = document.querySelector(".start-button");
 const firstNumber = document.querySelector(".first-number");
-const secondNumber = document.querySelector(".second-number")
+const secondNumber = document.querySelector(".second-number");
 const answer = document.querySelector(".answer");
+const correctOrWrong = document.querySelector(".correct-or-wrong");
+const answerBtn = document.querySelector(".answerBtn");
+const myAnswer = document.querySelector(".user-answer");
+const scoreNumber = document.querySelector(".score-number");
+const highscoreNumber = document.querySelector(".highscore-number");
+const body = document.querySelector("body");
+
+let score = 0;
+let highscore = 5;
+scoreNumber.innerHTML = score;
+highscoreNumber.innerHTML = highscore;
+
+function WrongAnswer() {
+  body.classList.add("wrong");
+
+  setTimeout(function () {
+    body.classList.remove("wrong");
+  }, 100);
+}
 
 function AnswerGame(equals) {
-    const answerBtn = document.querySelector(".answerBtn");
-    answerBtn.addEventListener("click", function(){
-        const correctOrWrong = document.querySelector(".correct-or-wrong");
-        
-        let userAnswer = Number(answer.value);
-        if(userAnswer === equals) {
-            correctOrWrong.innerHTML = String("CORRECT");
-            StartGame();
-        } else {
-            correctOrWrong.innerHTML = String("WRONG");
-        }
-    
-    })
+  answerBtn.addEventListener("click", function () {
+    let userAnswer = Number(answer.value);
+
+    myAnswer.innerHTML = userAnswer;
+    if (userAnswer === equals) {
+      score++;
+      scoreNumber.innerHTML = score;
+
+      answer.value = "";
+      if (score > highscore) {
+        highscore.innerHTML = score;
+        highscoreNumber.innerHTML = score;
+      }
+      StartGame();
+    } else {
+      WrongAnswer();
+    }
+  });
 }
 
 function GameTimer() {
-    let time = 120;
-    function tick() {
-        time --;
+  let time = 120;
+  function tick() {
+    time--;
 
-        Timer.innerHTML = Number(time);
+    Timer.innerHTML = Number(time);
 
-        if(time > 0){
-            setTimeout(tick, 1000)
-        } else {
-            Timer.innerHTML = String("TIME IS UP");
-        }}
-        tick();
+    if (time > 0) {
+      setTimeout(tick, 1000);
+    } else {
+      Timer.innerHTML = String("TIME IS UP");
+    }
+  }
+  tick();
 }
 
 function StartGame() {
-    
-    // RANDOM NUMBER
-    first = Number(Math.floor(Math.random() * 5));
-    second = Number(Math.floor(Math.random() * 5));
+  // RANDOM NUMBER
+  first = Number(Math.floor(Math.random() * 25));
+  second = Number(Math.floor(Math.random() * 25));
 
-    // ANSWER
-    let equals = first + second;
-    firstNumber.innerHTML = first;
-    secondNumber.innerHTML = second;
-    AnswerGame(equals);
-    GameTimer();
-    
+  // ANSWER
+  let equals = first + second;
+  firstNumber.innerHTML = first;
+  secondNumber.innerHTML = second;
+
+  AnswerGame(equals);
 }
 
-startBtn.addEventListener("click", function() {
-    StartGame()
-    }
-)
-
-
-    
-
+startBtn.addEventListener("click", function () {
+  GameTimer();
+  StartGame();
+});
